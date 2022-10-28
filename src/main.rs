e14f9d63604300613baebd89eaf89b26d191b01d
sudeshsubedi13 <078bct088@student.ioepc.edu.np>
2022-10-28 18:06:58 +0545

    first problem

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod solutions;
 
+
 fn main() {
-    println!("Hello, world!");
+    let upper: usize = 1000;
+    println!("sum of multiple of 3 and 5 below {}: {}", upper, solutions::multiples_of_3_and_5::sum_of_multiple_of_three_five(upper));
 }
